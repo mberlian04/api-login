ad3ea81b0636be79a537eb3f0d05b2bd9b98de10
mberlian04 <mberlian406@gmail.com>
2026-07-15 14:56:44 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,28 @@
 <?php
-include 'koneksi.php';
+include "koneksi.php";
 
-if(isset($_GET['username']) && isset($_GET['password'])){
-    $username = $_GET['username'];
-    $password = $_GET['password'];
+// UBAH DARI $_GET MENJADI $_POST
+if(isset($_POST['username']) && isset($_POST['password'])){
 
-    $query = $koneksi->prepare("SELECT * FROM user WHERE username = :user AND password = :pass");
-    $query->execute(['user' => $username, 'pass' => $password]);
-    $data = $query->fetch();
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // cek username dan password
+    $sql = "SELECT * FROM users 
+            WHERE username = :username 
+            AND password = :password";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([
+        ':username' => $username,
+        ':password' => $password
+    ]);
+
+    $data = $stmt->fetch();
 
     if($data){
         echo "berhasil";
-    } else {
+    }else{
         echo "gagal";
     }
 }
